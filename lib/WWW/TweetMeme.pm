a744ea8b7d52c733c56d7a1a0a3a1cf0d1cd791e
Adam Taylor <adamjctaylor@gmail.com>
2011-08-23 09:05:00 +0100

    Added documentation

--- lib/WWW/TweetMeme.pm
+++ lib/WWW/TweetMeme.pm
@@ -1,6 +1,21 @@
 # ABSTRACT: Perl wrapper for the TweetMeme API
 package WWW::TweetMeme;
 
+=head1 SYNOPSIS
+
+    use WWW::TweetMeme;
+    my $response = WWW::TweetMeme->get_url_info({ url => $url });
+
+=head1 DESCRIPTION
+
+Simple Perl interface to the TweetMeme core API that provides information
+about a URL, such as the number of times that URL has been shared on
+Twitter.
+
+=head1 METHODS
+
+=cut
+
 use strict;
 use warnings;
 
@@ -9,6 +24,14 @@ use LWP::UserAgent;
 use JSON;
 
 my $API_BASE = 'http://api.tweetmeme.com/';
+
+=head2 C<get_url_info>
+
+    my $response = WWW::TweetMeme->get_url_info({ url => $url });
+
+Returns a JSON decoded hash of information about the URL.
+
+=cut
 
 sub get_url_info {
     my ( $class, $args ) = @_;
@@ -40,4 +63,15 @@ sub _build_ua {
     return $ua;
 
 }
+
+=head1 SEE ALSO
+
+L<http://help.tweetmeme.com/category/developers/api/>
+
+=head1 AUTHOR
+
+Adam Taylor <ajct@cpan.org>
+
+=cut
+
 1;
